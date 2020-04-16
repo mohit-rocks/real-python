f9a19ef434f1cb393a5fba9ea9e5e08da54e12eb
Mohit Aghera <mohit.aghera@gmail.com>
2020-04-16 09:48:50 +0530

    Initialize python 3 virtual env.

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/mohit/Experiments/python-test/env"
+set -gx VIRTUAL_ENV "/Users/mohit/Experiments/real-python/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
